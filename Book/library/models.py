044196bdc7fc92eb35c167316ccab0b5d61902bf
mirza04 <mirza.munawar@ericsson.com>
2017-07-24 15:27:13 +0100

    Library Server

---
+++ Book/library/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Book(models.Model):
+
+    imageId = models.FileField()
+    isbn = models.CharField(max_length=200)
+    bookName = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    page = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.Book
